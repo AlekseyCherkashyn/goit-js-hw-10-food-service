67ee2fb8a9344b41f23e509fff738e996b40cc2e
AlekseyCherkashyn <alekseycherkashyn@gmail.com>
2021-09-04 23:44:46 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-// ----------------------------------------------------------------------------
-// --------------------------------- ИМПОРТЫ ----------------------------------
-// ----------------------------------------------------------------------------
-
-import db from './db/db.json'
-import templ from './templates/templ.hbs'
-import refs from './js/refs'
-import './js/func'
-import './js/listeners'
+import './js/menu'
+import './js/theme'
